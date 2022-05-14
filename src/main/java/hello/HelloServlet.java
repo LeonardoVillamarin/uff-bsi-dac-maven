cdb282904f727ef42b36ae952ab1c9940f5f08ec
Leonardo Villamarin <lvillamarin@id.uff.br>
2022-05-14 16:30:45 -0300

    Correcoes

--- src/main/java/hello/HelloServlet.java
+++ src/main/java/hello/HelloServlet.java
@@ -6,13 +6,15 @@
 package hello;
 import java.io.IOException;
 import java.io.PrintWriter;
+
+import javax.json.JsonObject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Calendar;
-import org.json.JSONObject;
+import org.json.JSONObject;  
 
 /**
  *
@@ -73,7 +75,7 @@ public class HelloServlet extends HttpServlet {
         
         
         String string_json = "{\"pt\":{\"greeting\":\"Olá\",\"morning\":\"bomdia\",\"afternoon\":\"boatarde\",\"evening\":\"boanoite\",\"sr\":\"Sr.\",\"sra\":\"Sra.\",\"none\":\"\"},\"en\":{\"greeting\":\"Hello\",\"morning\":\"goodmorning\",\"afternoon\":\"goodafternoon\",\"evening\":\"goodevening\",\"sr\":\"Mr.\",\"sra\":\"Mrs\",\"none\":\"\"},\"fr\":{\"greeting\":\"Bonjour\",\"morning\":\"bonmatin\",\"afternoon\":\"bonaprès-midi\",\"evening\":\"bonnenuit\",\"sr\":\"Monsieur\",\"sra\":\"Mme\",\"none\":\"\"},\"de\":{\"greeting\":\"Hallo\",\"morning\":\"gutenmorgen\",\"afternoon\":\"gutentag\",\"evening\":\"gutenacht\",\"sr\":\"Herr\",\"sra\":\"Frau\",\"none\":\"\"},\"esp\":{\"greeting\":\"Hola\",\"morning\":\"buendía\",\"afternoon\":\"buenastardes\",\"evening\":\"buenasnoches\",\"sr\":\"señor\",\"sra\":\"Sra.\",\"none\":\"\"},\"hav\":{\"greeting\":\"Aloha\",\"morning\":\"alohakakahiaka\",\"afternoon\":\"alohaawakea,alohaAuinalā\",\"evening\":\"alohaahiahi\",\"sr\":\"'OMr\",\"sra\":\"Uaha'iaku'oMrs\",\"none\":\"\"},}";
-        var json = new JSONObject(string_json);
+        JSONObject json = new JSONObject(string_json);
         String msg = "";
         
         String lang = request.getParameter("lang");
